7d2c0cb7a9c0f0b07117f252fd6c4322c26fcea2
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-14 04:30:53 -0300

    added latte test

---
+++ src/test/java/com/example/demo/LatteTest.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+import com.example.demo.controller.BrewerController;
+import com.example.demo.model.Coffee;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LatteTest {
+  @Autowired
+  BrewerController brewerController;
+  @Autowired
+  private MockMvc mockMvc;
+  private static ObjectWriter writer;
+  @BeforeAll
+  public static void setUp() {
+    writer = new ObjectMapper()
+            .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+            .writer().withDefaultPrettyPrinter();
+  }
+
+  @Test
+  void shouldReturnLatte() throws Exception {
+    //arrange
+    Coffee coffee = new Latte();
+    String json = writer.writeValueAsString(coffee);
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+            .get("/coffee/{name}", "latte");
+
+    ResultMatcher expectedStatus = status().isOk();
+    ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
+    ResultMatcher expectedBody = content().json(json);
+    //act and assert
+    mockMvc.perform(request)
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(expectedContentType)
+            .andExpect(expectedBody)
+            .andExpect(expectedStatus);
+
+  }
+}
